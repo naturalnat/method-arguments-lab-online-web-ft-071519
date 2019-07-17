2a009b4085e89acf515fb37a3e9af2bfaeb85cc3
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 16:28:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,10 @@ name = "Josh"
 puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language)
+def introduction_with_language(name, language)    
 name = "Natalie"
 language = "Ruby"
-  puts "Hi, my name is #{name} and I am learing to program in #{langauge}."
+
+puts "Hi, my name is #{name} and I am learing to program in #{langauge}."
+
 end
